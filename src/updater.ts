c8d986b0d0523c8e5bc45a6a3cd2466f477793e3
shane keulen <shane.keulen@gmail.com>
2026-02-09 18:59:06 -0500

    bump version to 0.0.12

--- src/updater.ts
+++ src/updater.ts
@@ -4,7 +4,7 @@ import { existsSync, mkdirSync, renameSync, unlinkSync, chmodSync } from "fs";
 import { dirname, resolve } from "path";
 
 const REPO = "SeeThruHead/cra-payroll";
-const VERSION = "0.0.11";
+const VERSION = "0.0.12";
 
 export const currentVersion = (): string => VERSION;
 
